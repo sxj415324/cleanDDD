205bad6be0557910f2d0a1b8f7b41bf39231cecc
sxj415324@163.com <sxj415324@163.com>
2020-09-24 10:38:24 +0800

    commit

---
+++ cleanDDD/src/main/java/com/SchemaFactory.java
@@ -0,0 +1,18 @@
+package com;
+
+import com.Schema;
+
+public class SchemaFactory {
+
+    public static Schema create(String flag) throws Exception {
+        switch(flag){
+            case "l" :
+                return new Schema(flag,"boolean",false);
+            case "p" :
+                return new Schema(flag,"int",0);
+            case "d" :
+                return new Schema(flag,"String","");
+        }
+        throw new Exception("value不能以-开头");
+    }
+}
